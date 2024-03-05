d1d3d4162bfc5c1062996c09b4cb338804e3ce1d
assembly-winston <153486593+assembly-winston@users.noreply.github.com>
2024-03-05 12:17:10 -0800

    ASM-29: Next-gen Project: Initial setup (#1)
    Co-authored-by: Winston Purnomo <winstonpurnomo@gmail.com>

---
+++ target/debug/deps/test_repo-2b2215a3417ad5fc.d
@@ -0,0 +1,5 @@
+/Users/winston/Developer/test-repo/target/debug/deps/test_repo-2b2215a3417ad5fc: src/main.rs
+
+/Users/winston/Developer/test-repo/target/debug/deps/test_repo-2b2215a3417ad5fc.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/test-repo.d
@@ -0,0 +1,1 @@
+/Users/winston/Developer/test-repo/target/debug/test-repo: /Users/winston/Developer/test-repo/src/main.rs
